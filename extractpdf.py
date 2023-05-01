029fe67fd7631d4e6640bffbfd5df6c470884c68
Mohd-PH <9362157+Mohd-PH@users.noreply.github.com>
2023-05-01 15:02:38 +0300

    fix name

--- extractpdf.py
+++ extractpdf.py
@@ -17,8 +17,8 @@ def split(inputFileName, outputFileName, startPage, endPage):
 
 def main():
     parser = argparse.ArgumentParser(
-            prog="split-pdf",
-            description="Split a pdf file by pages\npy extractpdf.py ./inputfile.pdf 1-20 ./outputfile.pdf",
+            prog="extract-pdf-pages",
+            description="Extract pages from a pdf file\npy extractpdf.py ./inputfile.pdf 1-20 ./outputfile.pdf",
         )
     parser.add_argument("inputFileName", help = "Input file");
     parser.add_argument("pages", help = "Pages to extract, first page(inclusive)-last page(inclusive), eg: 1-20");
